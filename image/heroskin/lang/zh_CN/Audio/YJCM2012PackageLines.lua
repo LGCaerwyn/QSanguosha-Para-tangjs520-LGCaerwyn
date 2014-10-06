91b67b67420df4b97dfb01e36d1dff41e72dfbc4
LGCaerwyn <1530491720@qq.com>
2014-10-06 22:08:25 +0800

    image(except heroskin and emotion)

---
+++ image/heroskin/lang/zh_CN/Audio/YJCM2012PackageLines.lua
@@ -0,0 +1,84 @@
+﻿-- lines for YJCM2012 Package
+
+return {
+	-- 步练师
+	["cv:bulianshi_1"] = "蒲小猫",
+	["$anxu1-bulianshi_1"] = "弥缝事体，安恤人言。",
+	["$anxu2-bulianshi_1"] = "夫君，朝事当量之，不可轻率！", -- 对孙权
+	["$zhuiyi1-bulianshi_1"] = "此情常在，追思不忘。",
+	["$zhuiyi2-bulianshi_1"] = "吾若有灵，常待君侧。", -- 对孙权
+	["~bulianshi-bulianshi_1"] = "陛下……今生情未了……来世再侍君……",
+
+
+        ["$anxu1-bulianshi_2"]     = "不急不急，与我共饮一杯。",
+	["$anxu2-bulianshi_2"]     = "莫慌莫慌，且听臣妾一曲", 
+	["$zhuiyi1-bulianshi_2"]   = "我走后，你要善待自己。",
+	["$zhuiyi2-bulianshi_2"]   = "忘记臣妾吧，好好生活下去。", 
+	["~bulianshi-bulianshi_2"] = "江之永矣……不可方思……",
+
+
+        
+
+	-- 曹彰
+	["$jiangchi1"] = "谨遵父训，不可逞匹夫之勇！", --额外摸一张牌
+	["$jiangchi2"] = "吾定当身先士卒，震魏武雄风！", --少摸一张牌
+	["~caozhang"] = "子桓……你害我……",
+
+	-- 程普
+	["$lihuo1"] = "将士们，引火对敌！",
+	["$lihuo2"] = "和我同归于尽吧！",
+	["$chunlao1"] = "唉，帐中不可无酒啊！",
+	["$chunlao2"] = "无碍，且饮一杯！",
+	["$chunlao3"] = "无碍，且饮一杯！",
+	["~chengpu"] = "没……没有酒了……",
+
+	-- 关兴张苞
+	["$fuhun1-guanxingzhangbao_1"] = "匹夫之勇。擦标卖首！", -- 使用【杀】
+	["$fuhun2-guanxingzhangbao_1"] = "光复汉室，重任在肩！", -- 获得技能
+	["~guanxingzhangbao-guanxingzhangbao_1"] = "未能手刃仇敌，愧对先父",
+
+
+        -- 韩当
+	["$gongqi1-handang_2"] = "龙驹陷阵，神弓破敌！",
+	["$gongqi2-handang_2"] = "看我箭弩弓张，取你性命！",
+	["$jiefan1-handang_2"] = "排愁消烦忧,祛害避凶邪",
+	["$jiefan2-handang_2"] = "公且放心，这里有我。",
+	["~handang-handang_2"] = "臣将战死，难为君王解忧了",
+
+	-- 华雄
+	["$shiyong1"] = "好大一股杀气啊！",--被红杀伤害
+	["$shiyong2"] = "好大一股酒气啊！",--被酒杀伤害
+	["~huaxiong"] = "皮厚……不挡刀啊……",
+
+	-- 廖化
+	["$dangxian1"] = "看我先行破敌！",
+	["$dangxian2"] = "先锋就由老夫来当！",
+	["$fuli1"] = "今天是个拼命的好日子！",
+	["$fuli2"] = "有老夫在，蜀汉就不会倒下！",
+	["~liaohua"] = "今后……就靠你们了……",
+
+	-- 刘表
+	["$zishou1"] = "江河霸主，何惧之有！",
+	["$zishou2"] = "荆襄之地，固若金汤！",
+	["~liubiao"] = "优柔寡断……要不得……",
+
+	-- 马岱
+	["$qianxi1"] = "喊什么喊，我敢杀你！",
+	["$qianxi2"] = "笑什么笑，叫你得意！",
+	["~madai"] = "我怎么会……死在这里……",
+
+	-- 王异
+	["$zhenlie1-wangyi_1"] = "休要小看妇人志气", 
+	["$zhenlie2-wangyi_1"] = "我决不屈服",
+	["$miji1-wangyi_1"] = "我将尽我所能",
+	["$miji2-wangyi_1"] = "奇谋只在绝境中诞生", -- 给牌后
+	["~wangyi-wangyi_1"] = "忠义已尽，死又何妨",
+
+	-- 荀攸
+	["$qice1-xunyou_2"] = "臣有一计，定能一攻而破！",
+	["$qice2-xunyou_2"] = "方今天下大乱，智士劳心之时也！",
+	["$zhiyu1-xunyou_2"] = "将军苦苦追逼，是为何故？",
+	["$zhiyu2-xunyou_2"] = "智可及，愚不可及。",
+	["~xunyou-xunyou_2"] = "主公何日再得无忧。",
+
+}
